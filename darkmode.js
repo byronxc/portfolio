5bb4db23dc1008a9e3461d3138038b9d9002277f
byronxc <bapineda22@gmail.com>
2020-01-02 13:56:30 -0800

    Upload files

---
+++ darkmode.js
@@ -0,0 +1,8 @@
+function darkMode() {
+    var element = document.getElementById("container")
+    element.classList.toggle("dark-mode");
+    var element1 = document.getElementById("dark")
+    element1.classList.toggle("dark-links");
+    var element2 = document.getElementById("dark1")
+    element2.classList.toggle("dark-links");
+}
