04f47484d9b6b3272c958a379cfbd2336daddaae
Byron <47398200+byronxc@users.noreply.github.com>
2021-02-23 13:26:18 -0800

    did some clean up

--- darkmode.js
+++ darkmode.js
@@ -1,8 +1,4 @@
 function darkMode() {
     var element = document.getElementById("container")
     element.classList.toggle("dark-mode");
-    var element1 = document.getElementById("dark")
-    element1.classList.toggle("dark-links");
-    var element2 = document.getElementById("dark1")
-    element2.classList.toggle("dark-links");
 }
